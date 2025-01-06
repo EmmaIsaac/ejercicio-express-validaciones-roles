461e09e83e39c5394c42bdeb980781082801f516
Emmanuel <emmanuel@MacBook-Pro-de-Emmanuel.local>
2025-01-06 19:18:17 -0300

    add: uploads

--- src/models/server.js
+++ src/models/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("../database/config");
-const { buscar } = require("../controllers/buscar");
+
+const fileUpload = require("express-fileupload");
 class Server {
   constructor() {
     this.app = express();
@@ -13,6 +14,7 @@ class Server {
       categorias: "/api/categorias",
       productos: "/api/productos",
       buscar: "/api/buscar",
+      uploads: "/api/uploads",
     };
 
     //Conectar a base de datos
@@ -32,6 +34,12 @@ class Server {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.static("public"));
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+      })
+    );
   }
 
   routes() {
@@ -40,6 +48,7 @@ class Server {
     this.app.use(this.paths.categorias, require("../routes/categorias"));
     this.app.use(this.paths.productos, require("../routes/productos"));
     this.app.use(this.paths.buscar, require("../routes/buscar"));
+    this.app.use(this.paths.uploads, require("../routes/uploads"));
   }
 
   listen() {
